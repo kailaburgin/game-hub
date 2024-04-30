cec26e2f20350a7c25d6d31644c4e37c1f1a6682
Kaila Burgin <kailaburgin@gmail.com>
2024-04-30 16:10:13 -0400

    display genres, fix loading skeleton, update card layout

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,18 +9,18 @@ const GameGrid = () => {
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer>
-            <GameCardSkeleton key={skeleton} />
-          </GameCardContainer>
-        ))}
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
